33b872553c200b9fd2cab08a4b2074d3b4fb0de0
Bogdan Shevchenko <bogdan@MacBook-Air-Bogdan.local>
2022-03-11 03:41:24 +0200

    Adaptiv

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ $(function() {
     $('.carusel__inner').slick({
         arrows: false,
         dots: true,
-        slidesToShow: 3
+        slidesToShow: 3,
+        responsive: [{
+            breakpoint: 841,
+            settings: {
+                slidesToShow: 2,
+            },
+            breakpoint: 601,
+            settings: {
+                slidesToShow: 1,
+            }
+
+        }]
     });
 });
